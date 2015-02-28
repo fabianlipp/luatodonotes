fabianlipp/luatodonotes

commit 6959adcaf706d12f0a0ccb429942d358a0f27b36

Fix wrong linespacing when changing fontsize

luatodonotes.dtx
@@ -73,6 +73,7 @@
 % \changes{0.2}{2014/09/08}{Compatibility with csquotes package (notes were
 % displayed multiple times when used in \texttt{\textbackslash blockquote}
 % command)}
+% \changes{0.2}{2015/01/13}{Fix wrong linespacing when changing fontsize}
 % \GetFileInfo{luatodonotes.dtx}   
 %
 % \DoNotIndex{\newcommand,\newenvironment}

luatodonotes.lua
@@ -257,6 +257,7 @@ function addNoteToList(index, drawLeader, noteType)
     newNote.backgroundColor = tex.toks["@todonotes@toks@currentbackgroundcolor"]
     newNote.borderColor = tex.toks["@todonotes@toks@currentbordercolor"]
     newNote.leaderWidth = tex.toks["@todonotes@toks@currentleaderwidth"]
+    newNote.sizeCommand = tex.toks["@todonotes@toks@sizecommand"]
     newNote.drawLeader = drawLeader
     if noteType == "area" then
         newNote.noteType = "area"
@@ -528,13 +529,15 @@ function printNotes()
             print("backgroundColor:" .. v.backgroundColor)
             print("borderColor:  " .. v.borderColor)
             print("leaderWidth:  " .. v.leaderWidth)
+            print("sizeCommand:  " .. v.sizeCommand)
             print("drawLeader:   " .. tostring(v.drawLeader))
         end
 
         -- print note
         tex.print(catcodeStart)
         tex.print("\\node[@todonotes@notestyle,anchor=north west," ..
-            "fill=" .. v.backgroundColor .. ",draw=" .. v.borderColor .. "] " ..
+            "fill=" .. v.backgroundColor .. ",draw=" .. v.borderColor .. "," ..
+            "font=" .. v.sizeCommand .. "] " ..
             "(@todonotes@" .. v.index ..
             " note) at (" .. v.outputX .. "sp," .. v.outputY .. "sp) {" ..
             v:boxForNoteText(v.rightSide) .. "};")

Makefile
@@ -34,7 +34,7 @@ luatodonotes.pdf: luatodonotes.sty luatodonotes.dtx
 	cp path_line.lua luatodonotes/path_line.lua
 	cp path_point.lua luatodonotes/path_point.lua
 	cp inspect.lua luatodonotes/inspect.lua
-	cp README luatodonotes/README
+	cp README.md luatodonotes/README
 	zip -r luatodonotes.zip luatodonotes
 
 luatodonotes.sty: luatodonotes.ins luatodonotes.dtx

commit d0698467f206eb9bc334e1c8148083ac1e54911d

Correct height calculation for notes with modified fontsize Bug introduced with commit 6959adc

luatodonotes.dtx
@@ -50,7 +50,7 @@
 %</driver>
 % \fi
 %
-% \CheckSum{851}
+% \CheckSum{825}
 %
 % \CharacterTable
 %  {Upper-case    \A\B\C\D\E\F\G\H\I\J\K\L\M\N\O\P\Q\R\S\T\U\V\W\X\Y\Z
@@ -77,6 +77,8 @@
 % \changes{0.2}{2015/01/13}{Included suggestions from CTAN submission}
 % \changes{0.2}{2015/02/28}{Make Lua variables and functions local or put them
 % into luatodonotes array (don't pollute global namespace)}
+% \changes{0.2}{2015/02/28}{Correct height calculation for notes with modified
+% fontsize}
 % \GetFileInfo{luatodonotes.dtx}
 %
 % \DoNotIndex{\newcommand,\newenvironment}

luatodonotes.lua
@@ -113,6 +113,7 @@ local notesForPage = luatodonotes.notesForPage
 -- backgroundColor: color of background of note
 -- borderColor: color of border of note
 -- leaderWidth: width of leader (used as argument for tikz line width)
+-- sizeCommand: fontsize command given as parameter for this note
 --
 -- Additional fields for text area:
 -- noteType: constant string "area"
@@ -465,7 +466,7 @@ function luatodonotes.calcHeightsForNotes()
 
         -- left side
         tex.sprint("\\savebox{\\@todonotes@heightcalcbox}" ..
-            "{" .. v:boxForNoteText(false) .. "}")
+            "{" .. v.sizeCommand .. v:boxForNoteText(false) .. "}")
         tex.sprint("\\@todonotes@heightcalcboxdepth=\\dp\\@todonotes@heightcalcbox")
         tex.sprint("\\@todonotes@heightcalcboxheight=\\ht\\@todonotes@heightcalcbox")
         tex.sprint("\\directlua{luatodonotes.notesForPage[" .. k .. "].heightLeft = " ..
@@ -474,7 +475,7 @@ function luatodonotes.calcHeightsForNotes()
 
         -- right side
         tex.sprint("\\savebox{\\@todonotes@heightcalcbox}" ..
-            "{" .. v:boxForNoteText(true) .. "}")
+            "{" .. v.sizeCommand .. v:boxForNoteText(true) .. "}")
         tex.sprint("\\@todonotes@heightcalcboxdepth=\\dp\\@todonotes@heightcalcbox")
         tex.sprint("\\@todonotes@heightcalcboxheight=\\ht\\@todonotes@heightcalcbox")
         tex.sprint("\\directlua{luatodonotes.notesForPage[" .. k .. "].heightRight = " ..